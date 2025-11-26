b4cdec5bfa115d87e052a9fba7a0ac0f1564f8c4
PJ Fanning <pjfanning@users.noreply.github.com>
2025-11-26 10:37:37 +0100

    Update JarCheck.java

--- src/main/java/com/github/pjfanning/releasevalidator/JarCheck.java
+++ src/main/java/com/github/pjfanning/releasevalidator/JarCheck.java
@@ -1,7 +1,9 @@
 package com.github.pjfanning.releasevalidator;
 
 import java.io.File;
+import java.util.Enumeration;
 import java.util.jar.Attributes;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
@@ -12,6 +14,17 @@ class JarCheck {
             Manifest man = jarFile.getManifest();
             Attributes attrs = man.getMainAttributes();
             System.out.println(attrs.get(Attributes.Name.MANIFEST_VERSION));
+            Enumeration<JarEntry> entries = jarFile.entries();
+            boolean moduleInfoClassFound = false;
+            while (!moduleInfoClassFound && entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                if (entry.getName().endsWith("module-info.class")) {
+                    moduleInfoClassFound = true;
+                }
+            }
+            if (!moduleInfoClassFound) {
+                return String.format("No module-info.class found in %s", file.getAbsolutePath());
+            }
         } catch (Exception e) {
             return String.format("Failed to check jar %s: %s", file, e);
         }
